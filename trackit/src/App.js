820bdba1b54d0d6e69549dbfa627891082e8743a
Lateef Azeez <codelate18@gmail.com>
2022-01-01 20:23:00 -0700

    create the sidebar component

--- trackit/src/App.js
+++ trackit/src/App.js
@@ -10,12 +10,14 @@ import Project from "./pages/project/Project"
 import Login from "./pages/login/Login"
 import Signup from "./pages/signup/Signup"
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar"
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <div className="container">
+      <Sidebar />
+      <div className="container">
           <Navbar />
           <Routes>
             <Route exact path="/" element={<Dashboard />} />
