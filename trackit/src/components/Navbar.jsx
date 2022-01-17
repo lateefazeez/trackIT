71dbb04cb10df818ac48c2c9593ffc417ad9926b
Lateef Azeez <codelate18@gmail.com>
2022-01-16 20:00:53 -0700

    update user status at logout

--- trackit/src/components/Navbar.jsx
+++ trackit/src/components/Navbar.jsx
@@ -1,26 +1,44 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 // styles & images
-import "./Navbar.css"
-import Temple from "../assets/temple.svg"
+import "./Navbar.css";
+import Spinner from "./Spinner";
 
 const Navbar = () => {
-  return ( 
+  const { logout, isPending } = useLogout();
+
+  return (
     <div className="navbar">
       <ul>
         <li className="logo">
           <div className="trackit-logo"></div>
-          <span><Link to="/">trackIT</Link></span>
+          <span>
+            <Link to="/">trackIT</Link>
+          </span>
         </li>
 
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
         <li>
-          <button className="btn">Logout</button>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+        <li>
+          {!isPending && (
+            <button className="btn" onClick={logout}>
+              Logout
+            </button>
+          )}
+          {isPending && (
+            <button className="btn" disabled>
+              <Spinner />
+            </button>
+          )}
         </li>
       </ul>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Navbar;
