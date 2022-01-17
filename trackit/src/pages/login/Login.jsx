c70f00f3a74ee05eadb2d39addef1feead040b4c
Lateef Azeez <codelate18@gmail.com>
2022-01-16 20:20:10 -0700

    log in users

--- trackit/src/pages/login/Login.jsx
+++ trackit/src/pages/login/Login.jsx
@@ -1,13 +1,53 @@
-
+import { useLogin } from "../../hooks/useLogin";
+import Spinner from "../../components/Spinner";
 
 // styles
-import "./Login.css"
+import { useState } from "react";
+import "./Login.css";
 
 const Login = () => {
-  return ( 
-    <h2>Login</h2>
-   );
-}
- 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login, isPending, error } = useLogin();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email && password) {
+      login(email, password);
+    }
+  };
+
+  return (
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      {!isPending && <button className="btn">Login</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          <Spinner />
+        </button>
+      )}
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+};
+
 export default Login;
-<h2>Login</h2>
